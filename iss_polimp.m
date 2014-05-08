17b0d03a97d16bc73d61ef99d86eee72b1b68eb4
Alastair Pharo <asppsa@gmail.com>
2014-05-09 02:59:58 +1000

    Fix for unbounded controls

--- iss_polimp.m
+++ iss_polimp.m
@@ -44,8 +44,22 @@ function [UOptimal, Flags] = iss_polimp_each(StateNum, Value, DeltaFunction, ...
   StateVect=SnToSVec(StateNum,CodingVector,Dimension);
   StateVars=(StateVect-1).*Options.StateStepSize+StateLB;
 
+  UStart = Options.ControlUB;
+
+  % Check for infinite control values. Happens when there
+  % are no control bounds.
+  ub_idx = isinf(UStart);
+  if any(ub_idx)
+    UStart(ub_idx) = Options.ControlLB(ub_idx);
+    lb_idx = isinf(UStart);
+    
+    % Happens when both bounds are inf.  Choose a random number.
+    if any(lb_idx)
+      UStart(lb_idx) = inv(Options.TimeStep)*10; %rand([1,length(lb_idx)]);
+    end
+  end
 
   [UOptimal, Flags] = ...
-      Optimizer(Options.ControlUB, Value, StateVars, DeltaFunction, ...
+      Optimizer(UStart, Value, StateVars, DeltaFunction, ...
                 StageReturnFunction, StateLB, StateUB, Conf);
 end
